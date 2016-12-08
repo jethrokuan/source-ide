c41384aca0eafa6f067c0ec755483a8123f07997
Jethro Kuan <jethrokuan95@gmail.com>
2016-12-09 05:19:48 +0800

    Finalized Vagrant setup

--- bin/vagrant.sh
+++ bin/vagrant.sh
@@ -1,19 +1,15 @@
 #!/usr/bin/env bash
 sudo su
 
-if [ ! -d "/var/www "]
+if [ ! -d "/var/source" ]
 then 
     echo "beginning vagrant provisioning process"
     sudo apt-get install -y git-core curl
 
-    # Installing nvm
-    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
-    echo "source /home/vagrant/.nvm/nvm.sh" >> /home/vagrant/.profile
-    source /home/vagrant/.profile
+    # Installing nodejs
+    curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+    sudo apt-get install -y nodejs
 
-    nvm install 7.2.0
-    nvm alias default 7.2.0
-    
     # Adding GPG keys
     # Yarn
     curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
@@ -23,9 +19,9 @@ then
     sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
     echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 
-    sudo apt-get update && sudo apt-get install -y mongodb-org yarn
+    sudo apt-get update && sudo apt-get install -y mongodb-org yarn build-essential
 
-    ln -s /vagrant/www /var/www
-
-    yarn install && yarn start
+    ln -s /vagrant/source /var/source
+    
+    pushd /var/source && yarn install && yarn dev
 fi
