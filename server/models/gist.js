ce1b5bdca5ce3cec87a66fc1855f070d82c39b8b
Jethro Kuan <jethrokuan95@gmail.com>
2016-12-09 23:10:50 +0800

    create document on server

--- server/models/gist.js
+++ server/models/gist.js
@@ -1,10 +1,25 @@
 import mongoose from 'mongoose';
+import backend from '../db';
 
 const Schema = mongoose.Schema;
 
-const gistSchema = {
+const gistSchema = new Schema({
   owner: [{ type: Schema.Types.ObjectId, ref: 'User' }],
   participants: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-};
+});
+
+gistSchema.pre('save', function(next) {
+  if (this.isNew) {
+    const conn = backend.connect();
+    const doc = conn.get('gists', this._id);
+    doc.fetch((err) => {
+      if (err) throw err;
+      if (doc.type === null) {
+        doc.create('function hello() { return true; }');
+      }
+    });
+  }
+  next();
+});
 
 module.exports = mongoose.model('Gist', gistSchema);
--- server/routes/gist.js
+++ server/routes/gist.js
@@ -1,20 +1,11 @@
 import Router from 'koa-router';
-import backend from '../db';
+
 import authenticated from '../lib/middlewares/authenticated';
 
 import User from '../models/user';
 import Gist from '../models/gist';
 
 const router = new Router();
-
-const createIfNeeded = (doc) => {
-  doc.fetch((err) => {
-    if (err) throw err;
-    if (doc.type === null) {
-      doc.create('');
-    }
-  });
-};
 
 router.use(authenticated());
 
@@ -29,26 +20,22 @@ router.get('/gists', async (ctx) => {
 router.get('/gists/new', async (ctx) => {
   let gistid;
   const q = await User.findById(ctx.session.passport.user)
-        .exec((err, user) => {
+        .exec(async (err, user) => {
           if (err) throw err;
           const gist = new Gist({
             owner: user._id,
             participants: [],
           });
-          
-          gist.save();
-          user.gists.push(gist._id);
-          user.save();
           gistid = gist._id;
+          await gist.save();
+          user.gists.push(gist._id);
+          await user.save(); 
         });
 
   await ctx.redirect(`/gist/${gistid}`);
 });
 
 router.get('/gist/:id', async (ctx) => {
-  const conn = backend.connect();
-  const doc = conn.get('gists', ctx.params.id);
-  createIfNeeded(doc);
   await ctx.render('gists/show');
 });
 
