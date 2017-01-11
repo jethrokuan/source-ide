2d87b33e23fe44efb8f649e4b05773626ecb6b78
Jethro Kuan <jethrokuan95@gmail.com>
2017-01-11 15:57:28 +0800

    convert /gists/new to JSON response

--- server/routes/gist.js
+++ server/routes/gist.js
@@ -9,36 +9,46 @@ const router = new Router();
 
 router.use(authenticated());
 
+// TODO: Convert into JSON API
 router.get('/gists', async (ctx) => {
-  let user = await User.findById(ctx.session.passport.user)
-      .populate('gists')
-      .exec();
+  const user = await User.findById(ctx.session.passport.user)
+        .populate('gists')
+        .exec();
 
   await ctx.render('gists/list', { gists: user.gists });
 });
 
-router.get('/gists/new', async (ctx) => {
-  let gistid;
-  const q = await User.findById(ctx.session.passport.user)
-        .exec(async (err, user) => {
-          if (err) throw err;
-          const gist = new Gist({
-            owner: user._id,
-            participants: [],
-          });
-          gistid = gist._id;
-          await gist.save();
-          user.gists.push(gist._id);
-          await user.save();
+router.post('/gists/new', async (ctx) => {
+  try {
+    let gistid;
+    await User.findById(ctx.session.passport.user)
+      .exec(async (err, user) => {
+        if (err) throw err;
+        const gist = new Gist({
+          owner: user._id,
+          participants: [],
         });
-
-  await ctx.redirect(`/gist/${gistid}`);
+        gistid = gist._id;
+        await gist.save();
+        user.gists.push(gist._id);
+        await user.save();
+      });
+    ctx.body = {
+      status: 0,
+      id: gistid,
+    };
+  } catch (err) {
+    ctx.body = {
+      status: 1,
+      error: err,
+    };
+  }
 });
 
+// TODO: Convert to JSON API
 router.get('/gist/:id', async (ctx) => {
   const q = await Gist.findById(ctx.params.id).exec((err, gist) => {
-    if (err) throw err;
-    console.log(gist);
+    if (err) throw err; 
     ctx.state.codeId = gist.code;
     ctx.state.testcaseId = gist.testcase;
   });
